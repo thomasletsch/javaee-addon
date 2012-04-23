10d57fa171ce367ebee7fba1c6e7a74f224397c8
Thomas Letsch <contact@thomas-letsch.de>
2012-04-23 17:46:20 +0200

    Reworked property files

--- src/main/java/com/optible/vaadin/utils/i18n/CommonTranslations.java
+++ src/main/java/com/optible/vaadin/utils/i18n/CommonTranslations.java
@@ -2,7 +2,7 @@ package com.optible.vaadin.utils.i18n;
 
 public class CommonTranslations extends ResourceBundleTranslations {
 
-    static final String COMMON_BUNDLE_NAME = "CommonI18n";
+    static final String COMMON_BUNDLE_NAME = "Common";
 
     public CommonTranslations() {
         super(COMMON_BUNDLE_NAME);
