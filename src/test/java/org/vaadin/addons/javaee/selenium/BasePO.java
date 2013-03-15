d5e835223b12bc01e90c64a9438ea0b703b266e0
Thomas Letsch <contact@thomas-letsch.de>
2013-03-15 10:49:34 +0100

    Small test utils enhancements

--- src/test/java/org/vaadin/addons/javaee/selenium/BasePO.java
+++ src/test/java/org/vaadin/addons/javaee/selenium/BasePO.java
@@ -4,8 +4,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.vaadin.addons.javaee.selenium.input.InputMethodFactory;
 
 public abstract class BasePO {
+
+    protected final static boolean DO_NOT_ASSERT_PAGE = false;
+
+    public final static int SHORT_WAIT_SEC = 1;
 
     protected WebDriver driver;
 
@@ -17,16 +22,25 @@ public abstract class BasePO {
 
     protected DataDrivenAssertions dataAssertions;
 
+    protected InputMethodFactory inputMethodFactory;
+
     protected BasePO() {
     }
 
     public BasePO(WebDriver driver) {
+        this(driver, true);
+    }
+
+    protected BasePO(WebDriver driver, boolean assertPage) {
         this.driver = driver;
         actions = new SeleniumActions(driver);
         dataActions = new DataDrivenActions(actions);
         assertions = new SeleniumAssertions(driver);
         dataAssertions = new DataDrivenAssertions(assertions);
-        assertPage();
+        inputMethodFactory = new InputMethodFactory(driver);
+        if (assertPage) {
+            assertPage();
+        }
     }
 
     protected abstract String getIdentifyingElement();
